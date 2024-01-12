ce0197cd97853156d2f76aa3708e85e1d26c7c7d
Benjamin Brodwolf <benjaminglennberrie.brodwolf@calbaptist.edu>
2024-01-12 13:33:27 -0800

    fimal commit

---
+++ BinaryCounterOptimized.java
@@ -0,0 +1,55 @@
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.lang.InterruptedException;
+
+public class BinaryCounterOptimized {
+
+    public static void main(String[] args) {
+
+        // Create an array with a size of 52 like a deck of cards
+        int[] myArray = new int[8];
+
+        // Populate the array with numbers from 1 to 52
+        for (int i = 0; i < myArray.length; i++) {
+
+            Random random = new Random();
+
+            // Generate a random number that is either 0 or 1
+            int randomNumber = random.nextInt(2);
+            myArray[i] = randomNumber;
+
+        }
+
+        // Print the array
+        System.out.print("\nArray elements: ");
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.print(myArray[i] + " ");
+        }
+
+        int i = 0;
+
+        while (i <= myArray.length-1 && myArray[i] == 1) {
+            myArray[i] = 0;
+            i++;
+        }
+        if (i < myArray.length-1) {;
+            myArray[i] = 1;
+        }
+
+        // Print the array
+        System.out.print("\nArray elements: ");
+
+        for ( i = 0; i < myArray.length; i++) {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (Exception e) {
+                System.out.println("You broke it");
+            }
+
+            System.out.print(myArray[i] + " ");
+
+        }
+
+    }
+
+}
